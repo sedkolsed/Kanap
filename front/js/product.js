5bf843989f4459b518f6d1fcc19b3c4f8000d25e
sedkolsed <sedkolsed@gmail.com>
2022-05-18 22:41:04 +0200

    name price description colors

--- front/js/product.js
+++ front/js/product.js
@@ -23,6 +23,10 @@ function productData (kanap){
     const price = kanap.price;
     const description = kanap.description;
     imageKanap ( imageUrl, altTxt);
+    nameKanap(name);
+    priceKanap(price);
+    descriptionKanap(description);
+    colorsKanap(colors);
 
 }
 
@@ -33,4 +37,25 @@ function imageKanap (imageUrl, altTxt){
     const item__img = document.querySelector(".item__img");
     item__img.appendChild(img);
 }
+
+function nameKanap(name){
+    document.querySelector ("#title").innerHTML= name;
+}
+function priceKanap(price){
+    document.querySelector ("#price").innerHTML=price;
+}
+function descriptionKanap(description){
+    document.querySelector ("#description").innerHTML=description;
+
+}
+function colorsKanap(colors){
+    const select = document.querySelector ("#colors");
+     colors.forEach(element => {
+         const option = document.createElement ("option");
+         option.value = element;
+         option.innerHTML =element;
+         select.appendChild(option);       
+    });
+} 
+
 
