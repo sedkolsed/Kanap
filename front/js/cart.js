d8a1af5ad027ae667e63d5028039f5b76ea32b31
sedkolsed <sedkolsed@gmail.com>
2022-05-23 00:39:23 +0200

    articles in cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,40 @@
 const itemsInStorage = localStorage.length;
 console.log("il y en a :" ,itemsInStorage) ;
+const orderList = [];
+
+
+for (let i = 0; i < itemsInStorage ; i++ ){
+    const item = localStorage.getItem(localStorage.key(i));
+    const itemObject = JSON.parse(item);
+    orderList.push(itemObject);
+
+}
+console.log(orderList);
+
+let products = document.querySelector("#cart__items");
+
+for (let article of orderList) {
+   
+    products.innerHTML += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+                <div class="cart__item__img">
+                  <img src="${article.imageUrl}" alt=${article.altTxt}>
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${article.name}</h2>
+                    <p>${article.color}</p>
+                    <p>${article.price} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${article.quantity}>
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article> `}
+
+              
