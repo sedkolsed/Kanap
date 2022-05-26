16b84cdd99576b04d506c1e69b4e1854ee9b4e31
sedkolsed <sedkolsed@gmail.com>
2022-05-26 19:27:40 +0200

    pointage de listenQuantity ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,17 +1,13 @@
 // taille du localStorage & création de orderList.....................................................
 const itemsInStorage = localStorage.length;
-console.log(itemsInStorage) ;
+console.log(itemsInStorage);
 const orderList = [];
 
-
-
-
 // boucle des objets de localStorage vers orderList..............................................
-for (let i = 0; i < itemsInStorage ; i++ ){
+for (let i = 0; i < itemsInStorage; i++) {
   const item = localStorage.getItem(localStorage.key(i));
   const itemObject = JSON.parse(item);
   orderList.push(itemObject);
-  
 }
 console.log(orderList);
 
@@ -19,8 +15,7 @@ console.log(orderList);
 let products = document.querySelector("#cart__items");
 
 for (let article of orderList) {
-  
-  products.innerHTML += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+  products.innerHTML += `<article class="cart__item" data-id="${article.productId}" data-color="${article.color}">
   <div class="cart__item__img">
   <img src="${article.imageUrl}" alt=${article.altTxt}>
   </div>
@@ -40,108 +35,94 @@ for (let article of orderList) {
   </div>
   </div>
   </div>
-  </article> `
-  
-  
+  </article> `;
 }
 
 // écoute sur les changements de quantités........................................
 
 listenQuantity();
 
-function listenQuantity (){
-  const changeQuantity = document.querySelector (".itemQuantity");
-  changeQuantity.addEventListener("input" ,  () => updateQuantity());
+function listenQuantity() {
+  const changeQuantity = document.querySelectorAll(".itemQuantity");
+  console.log(changeQuantity);
+  changeQuantity.forEach((section) => {
+    section.addEventListener("input", () => updateQuantity());
+  });
 }
 function updateQuantity() {
-  
-  
   console.log(orderList);
 }
 
 // suppression des articles....................................................
 
-
-
-
-
 // quantité totale des articles...............................................
 
 displayTotalQuantity();
 
-function displayTotalQuantity (){
-  let total = 0 ;
-  const totalQuantity = document.querySelector ("#totalQuantity");
+function displayTotalQuantity() {
+  let total = 0;
+  const totalQuantity = document.querySelector("#totalQuantity");
 
-  orderList.forEach((object)=> {
-    const totalQuantityObject = object.quantity  ;
+  orderList.forEach((object) => {
+    const totalQuantityObject = object.quantity;
     total += totalQuantityObject;
-  })
+  });
 
-    totalQuantity.textContent = total;
-  
+  totalQuantity.textContent = total;
 }
 
 // calcul du prix total........................................
- 
+
 displayTotalPrice();
 
-function displayTotalPrice(){
-  let total = 0 ;
-  const totalPrice = document.querySelector ("#totalPrice");
-  
-  orderList.forEach((object) => {  
+function displayTotalPrice() {
+  let total = 0;
+  const totalPrice = document.querySelector("#totalPrice");
+
+  orderList.forEach((object) => {
     const totalPriceByObject = object.price * object.quantity;
     total += totalPriceByObject;
-    });
+  });
 
-    totalPrice.textContent = total ;   
-  }
+  totalPrice.textContent = total;
+}
 
-  
-  
-  //  formulaire.........................................
+//  formulaire.........................................
 
-  const orderButton = document.querySelector("#order");
-  orderButton.addEventListener("click", (e) => storeForm(e));
+const orderButton = document.querySelector("#order");
+orderButton.addEventListener("click", (e) => storeForm(e));
 
-
-  function storeForm (e){
-    e.preventDefault ();
-    if (orderList.length === 0 ) alert("Votre panier est vide !");
-    const form = document.querySelector(".cart__order__form");
-    console.log(form.elements);
-    const dataForm = dataFormArray();
+function storeForm(e) {
+  e.preventDefault();
+  if (orderList.length === 0) alert("Votre panier est vide !");
+  const form = document.querySelector(".cart__order__form");
+  console.log(form.elements);
+  const dataForm = dataFormArray();
   //   fetch ("http://localhost:3000/api/products/order",{
   //   method : "POST",
-  //      headers: { 
-  //   'Accept': 'application/json', 
-  //   'Content-Type': 'application/json' 
+  //      headers: {
+  //   'Accept': 'application/json',
+  //   'Content-Type': 'application/json'
   //   },
   //     body: JSON.stringify(dataForm)
-      
-    
+
   // })
   //     .then((res)=>res.json())
   //     .then((data) => console.log(data))
-  
-   
-  }
+}
 
-  function dataFormArray(){
-    const dataOrder = {
-      contact :{
-        firstName : "a" ,
-        lastName : "b",
-        adress : "c",
-        city : "d",
-        email : "e"
-      },
-      products : ["998787879809808"]
+function dataFormArray() {
+  const dataOrder = {
+    contact: {
+      firstName: "a",
+      lastName: "b",
+      adress: "c",
+      city: "d",
+      email: "e",
+    },
+    products: ["998787879809808"],
+  };
+  return dataOrder;
+}
 
-    }
-    return dataOrder;
-  }
-
-
-  // validation du formulaire.....................................
+// validation du formulaire.....................................
--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,23 @@
 // requête sur les produits de l'api............................................................
-fetch ("http://localhost:3000/api/products")
-.then (function(res){
-    if (res.ok){
-        return res.json();
+fetch("http://localhost:3000/api/products")
+  .then(function (res) {
+    if (res.ok) {
+      return res.json();
     }
-})
-.then (function(value){
+  })
+  .then(function (value) {
     console.log(value);
     canapes(value);
-})
-.catch (function(erreur){
-    console.log ("une erreur est survenue");
-})
-
-
+  })
+  .catch(function (erreur) {
+    console.log("une erreur est survenue");
+  });
 
 // affichage de tous les articles..............................................................
 function canapes(articles) {
-  
   let products = document.querySelector("#items");
- 
+
   for (let article of articles) {
-   
     products.innerHTML += `<a href="./product.html?_id=${article._id}">
     <article>
         <img src="${article.imageUrl}" alt="${article.altTxt}">
